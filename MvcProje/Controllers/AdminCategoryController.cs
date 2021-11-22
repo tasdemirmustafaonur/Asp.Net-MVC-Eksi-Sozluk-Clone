ff024955c8023964eb1cce73cf1d77478eb9f895
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2021-11-22 14:16:18 +0300

    Category operations with sidebar

--- MvcProje/Controllers/AdminCategoryController.cs
+++ MvcProje/Controllers/AdminCategoryController.cs
@@ -3,14 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 
 namespace MvcProje.Controllers
 {
     public class AdminCategoryController : Controller
     {
-        // GET: AdminCategory
+        private CategoryManager cm = new CategoryManager(new EfCategoryDal());
         public ActionResult Index()
         {
+            var categoryValues = cm.GetList();
+            return View(categoryValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult results = categoryValidator.Validate(p);
+            if (results.IsValid)
+            {
+                cm.CategoryAddBL(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                        ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+                }
+            }
+
             return View();
         }
     }
