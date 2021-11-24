7931cf0cae58c1b4f6af20361809a388b4d7e57b
Mustafa Onur Tasdemir <mustafaonurtasdemirr@gmail.com>
2021-11-24 13:08:32 +0300

    Writer operations

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            //burası linq işlemleri midir. 30. video 6.10 dakika dalinq'dan bahsediyor.
+            RuleFor(x => x.WriterName).NotEmpty().WithMessage("Yazar adını Boş Geçemezsiniz.");
+            RuleFor(x => x.WriterSurname).NotEmpty().WithMessage("Yazar soyadını boş bırakamazsınız.");
+            RuleFor(x => x.WriterAbout).NotEmpty().WithMessage("Hakkımda kısmını boş bırakamazsınız.");
+            RuleFor(x => x.WriterSurname).MinimumLength(2).WithMessage("Lütfen en az 2 karakter girişi yapın");
+            RuleFor(x => x.WriterSurname).MaximumLength(50)
+                .WithMessage("Lütfen 50 karakterden fazla değer girişi yapmayın");
+            RuleFor(x => x.WriterTitle).NotEmpty().WithMessage("Unvan kısmını boş bırakamazsınız.");
+
+        }
+    }
+}
--- MvcProje/Controllers/WriterController.cs
+++ MvcProje/Controllers/WriterController.cs
@@ -4,17 +4,73 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 
 namespace MvcProje.Controllers
 {
     public class WriterController : Controller
     {
         private WriterManager wm = new WriterManager(new EfWriterDal());
+        WriterValidator writerValidator = new WriterValidator();
+
         public ActionResult Index()
         {
             var WriterValues = wm.GetList();
             return View(WriterValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddWriter()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddWriter(Writer p)
+        {
+            ValidationResult results = writerValidator.Validate(p);
+            if (results.IsValid)
+            {
+                wm.WriterAdd(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName,item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult EditWriter(int id)
+        {
+            var writerValue = wm.GetByID(id);
+            return View(writerValue);
+        }
+
+        [HttpPost]
+        public ActionResult EditWriter(Writer p)
+        {
+            ValidationResult results = writerValidator.Validate(p);
+            if (results.IsValid)
+            {
+                wm.WriterUpdate(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
         }
     }
 }
